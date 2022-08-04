1f249eac0c6c6a7195fedf7ee40a41a8cc2172fb
B4mbus <peaceofloneliness@gmail.com>
2022-08-04 17:14:22 +0200

    docs: add packspec.lua

---
+++ packspec.lua
@@ -0,0 +1,10 @@
+package = 'oxocarbon-lua.nvim'
+version = '0.1.0'
+source = 'git://github.com/b4mbus/oxocarbon-lua.nvim.git'
+description = {
+	summary = 'oxocarbon.nvim port to lua.'
+	detailed = [[
+		Provides a lua port of the oxocarbon.nvim colorscheme.
+	]]
+	license = 'MIT'
+}
